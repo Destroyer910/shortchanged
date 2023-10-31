3f4a440878f615cb6b1e1acd4e06b72dfd0219e4
Daniel Cook <addisoncook08@gmail.com>
2023-10-31 14:13:12 -0400

    Player Stats

---
+++ Shortchanged/Assets/Daniel/Scripts/PlayerManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int Health = 100;
+    public double JumpHeight = 0; // DEFINITELY CHANGE !!!
+    public double Speed = 10;
+    public double SprintSpeed = 15;
+    public double DetectionRadius = 1.0;
+}
