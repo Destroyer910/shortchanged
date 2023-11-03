0cad3923596a855a89e93bc1197753c460cfe0c8
LBriggs1101 <116574710+LBriggs1101@users.noreply.github.com>
2023-11-03 06:57:08 -0400

    did some of the interactable stuff
    fsadfdssfda

---
+++ Shortchanged/Assets/Scripts/Interactables/Doors/DoorOpen.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    
+    private GameObject parentObject;
+    private float originalRotation;
+    public float rotationSpeed;
+
+    void Start()
+    {
+        parentObject = transform.parent.gameObject;
+        originalRotation = transform.rotation.y;
+        toggleDoor();
+    }
+
+    public void toggleDoor()
+    {
+        Debug.Log("Start");
+        if(transform.rotation.y == originalRotation)
+        {
+            parentObject.transform.localEulerAngles = new Vector3(0, originalRotation + 90, 0);
+        }
+        else
+        {
+            parentObject.transform.localEulerAngles = new Vector3(0, originalRotation, 0);
+        }
+    }
+
+    
+}
--- Shortchanged/Assets/Scripts/Player/InteractWithStuff.cs
+++ Shortchanged/Assets/Scripts/Player/InteractWithStuff.cs
@@ -6,16 +6,29 @@ public class InteractWithStuff : MonoBehaviour
 {
 
     public int raycastDistance = 10; 
+    public int layer;
+
+    void Start()
+    {
+        layer = LayerMask.GetMask("Interactable");
+    }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
 
-        if (Physics.Raycast(transform.position, fwd, raycastDistance))
-        {
+        RaycastHit hit;
 
+        if (Physics.Raycast(transform.position, fwd, out hit, raycastDistance, layer))
+        {
+            if(hit.collider.tag == "Door")
+            {
+                
+            }
         }
-            
+
+        Debug.DrawRay(transform.position, fwd * hit.distance, Color.yellow);
+
     }
 }
