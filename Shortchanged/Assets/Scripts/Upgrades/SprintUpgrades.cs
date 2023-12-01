e6156b54c6c08ef21b374a5c387680a744314078
LBriggs1101 <116574710+LBriggs1101@users.noreply.github.com>
2023-12-01 06:48:44 -0500

    almost finished the shop
    yay

--- Shortchanged/Assets/Scripts/Upgrades/SprintUpgrades.cs
+++ Shortchanged/Assets/Scripts/Upgrades/SprintUpgrades.cs
@@ -9,15 +9,19 @@ public class SprintUpgrades : MonoBehaviour
     public string textForFailedBuy;
     public string textForBuy;
     public int cashCost;
+    public float reqSpeed;
     public float newSpeed;
     public GameObject visualUpgrade;
     private bool isDeactivated = false;
     public Material deactivatedMaterial;
+    public Material activatedMaterial;
     private Light theLight;
+    private Vector3 startPosition;
 
     private void Start() 
     {
-        theLight = visualUpgrade.GetComponent<Light>();    
+        theLight = visualUpgrade.GetComponent<Light>();  
+        startPosition = transform.position;  
     }
 
     public void upgradeSprintSpeed()
@@ -39,11 +43,32 @@ public class SprintUpgrades : MonoBehaviour
     {
         if(playerManagerScript.getSprintSpeed() >= newSpeed && !isDeactivated)
         {
-            visualUpgrade.GetComponent<MeshRenderer>().material = deactivatedMaterial;
-            theLight.color = Color.gray;
-            gameObject.transform.position = new Vector3(2000, 2000, 2000);
-            isDeactivated = true;
+            disableButton();
         }
+        if(playerManagerScript.getSprintSpeed() < reqSpeed)
+        {
+            disableButton();
+        }
+        else if(playerManagerScript.getSprintSpeed() == reqSpeed)
+        {
+            enableButton();
+        }
+    }
+
+    private void disableButton()
+    {
+        visualUpgrade.GetComponent<MeshRenderer>().material = deactivatedMaterial;
+        theLight.color = deactivatedMaterial.color;
+        gameObject.transform.position = new Vector3(2000, 2000, 2000);
+        isDeactivated = true;
+    }
+
+    private void enableButton()
+    {
+        visualUpgrade.GetComponent<MeshRenderer>().material = activatedMaterial;
+        theLight.color = activatedMaterial.color;
+        gameObject.transform.position = startPosition;
+        isDeactivated = false;
     }
 
     
